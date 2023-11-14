4f635cd6a3368299521dc74b92eeebf377acecbc
Zhuang Li <lizhuang144@gmail.com>
2023-11-14 22:07:16 +1100

    update files

--- examples/prune_data.py
+++
@@ -1,7 +0,0 @@
-line_set = set()
-for line in open(file='spice_input.txt').readlines():
-    line_set.add(line.strip().replace('p:','').replace(':1','').replace(':2','').replace(':3','').replace(':4',''))
-
-fp = open(file='prune_spice_output.txt', mode='w')
-for line in line_set:
-    fp.write(line+'\n')
