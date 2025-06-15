3834efc3c9ed8129f9fa81d932cefa7a4a40da2a
Zhuang Li <e129369@Mac.modem>
2025-06-16 03:35:24 +1000

    Release v0.7.2: Improve README with enhanced VLM context and descriptions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FactualSceneGraph',
-    version='0.7.1',
+    version='0.7.2',
     author='Zhuang Li',
     author_email='lizhuang144@gmail.com',
     description='A package for scene graph parsing and evaluation with multi-sentence support',
